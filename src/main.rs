605c23b7422ef092d6b439dd7cb9e4a3cab85989
Clover <39840298+SmollClover@users.noreply.github.com>
2024-12-24 23:54:54 +0100

    Learning Rust (i have no idea what I am doing)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let folder = args.get(1);
+
+    if folder.is_none() {
+        eprintln!("Please provide a folder path as an argument.");
+        return;
+    }
+
+    let path = Path::new(folder.unwrap());
+
+    if !path.is_dir() {
+        eprintln!("Please provide a folder path as an argument.");
+        return;
+    }
+
+    // print!("Reading directory {} : ", "tmp");
+
+    for file in path.read_dir().expect("Reading directory failed") {
+        match file {
+            Err(err) => eprintln!("Could not read file: {}", err),
+            Ok(file) => {
+                dbg!(file);
+            }
+        }
+    }
 }
